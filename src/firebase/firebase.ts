53056c9dcb99a6aa158fa18ae60e005c7d1cb3b3
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-07-30 14:39:40 -0300

    Inital commit
    Setup repo

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrgMcKXGZFDyUQnW8XInkYjxQbJlKBp8s",
+  authDomain: "contadorvueltas.firebaseapp.com",
+  projectId: "contadorvueltas",
+  storageBucket: "contadorvueltas.appspot.com",
+  messagingSenderId: "67235687713",
+  appId: "1:67235687713:web:ac3202196849a0a889a260"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
