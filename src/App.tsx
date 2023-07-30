7c6b1dd633859d6474bb89b3230fbaccc6fc72ec
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-07-30 20:30:45 -0300

    Create Basic functionality and add new dependencies
    * Chakra ui
    * Chakra Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {MainCounter} from "./components";
+import { Button } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-screen flex flex-col items-center justify-center bg-pastelLilac gap-10">
+        <MainCounter />
+    <div className="w-3/4">
+        <h2 className='text-xl text-slate-50 font-bold'>Secondary Counters</h2>
+        <Button sx={{background: "#D9D8A9", color: "white"}} size="sm" >New</Button>
+    </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,14 +3,21 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const APIKEY = import.meta.env.VITE_APIKEY;
+const AUTHDOMAIN = import.meta.env.VITE_AUTHDOMAIN;
+const PROJECTID = import.meta.env.VITE_PROJECTID;
+const STORAGEBUCKET = import.meta.env.VITE_STORAGEBUCKET;
+const MESSAGINSENDERID = import.meta.env.VITE_MESSAGINSENDERID;
+const APPID = import.meta.env.VITE_APPID;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrgMcKXGZFDyUQnW8XInkYjxQbJlKBp8s",
-  authDomain: "contadorvueltas.firebaseapp.com",
-  projectId: "contadorvueltas",
-  storageBucket: "contadorvueltas.appspot.com",
-  messagingSenderId: "67235687713",
-  appId: "1:67235687713:web:ac3202196849a0a889a260"
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINSENDERID,
+  appId: APPID,
 };
 
 // Initialize Firebase
